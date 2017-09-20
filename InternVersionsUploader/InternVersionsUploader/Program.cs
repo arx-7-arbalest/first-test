38850efa1de5a3865a33d19dffb14c2bd795b145
arx-7-arbalest <32123777+arx-7-arbalest@users.noreply.github.com>
2017-09-20 16:06:44 +0200

    Add files via upload

---
+++ InternVersionsUploader/InternVersionsUploader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternVersionsUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+    public class test
+    {
+
+    }
+
+    public class test2
+    {
+
+    }
+}
